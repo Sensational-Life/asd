dbcabfb5c6ea761c14d530dd6e0875e74459b847
Dilekcn <dcanturk1@gmail.com>
2022-01-22 02:45:17 +0000

    section 13 completed

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 
 import logo from "../logo.svg";
 import AboutUs from "./AboutUs/AboutUs";
+import FooterIcon from "./FooterIcon/FooterIcon";
 import Welcome from "./Welcome/Welcome";
 import WhatWeDo from "./WhatWeDo/WhatWeDo";
 
@@ -28,21 +29,12 @@ export function Home() {
 
 	return (
 		<main role="main">
-			<div> 
-				<img
-					className="logo"
-					data-qa="logo"
-					src={logo}
-					alt="Just the React logo"
-					
-				/>
-				{/* <h1 className="message" data-qa="message">
-					{message}
-				</h1>
-				<Link to="/about/this/site">About</Link> */}
-				<Welcome/>
-				<AboutUs/>
-				<WhatWeDo/>
+			<div>
+				{/*<Link to="/about/this/site">About</Link> */}
+				<Welcome />
+				<AboutUs />
+				<WhatWeDo />
+				<FooterIcon />
 			</div>
 		</main>
 	);
