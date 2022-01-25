d3aa169fa2e97dc4b0fa6a3d4add27b748c4e062
Megumi Yanaka <margaret_mori0102@yahoo.com>
2022-01-25 13:28:08 +0000

    add footer logo and button

---
+++ client/src/pages/Home/FooterLogo/FooterLogo.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./FooterLogo.css";
+
+export function Footer1() {
+	const [isShown, setIsShown] = useState(false);
+	return (
+		<div id="footerlogo">
+			<div className="footerlogo_container">
+				<div className="footer-logo-img_container">
+					<img
+						className="footer-logo-img"
+						src="https://i.postimg.cc/VkdMYP4S/Logo.jpg"
+						alt="logo"
+					/>
+				</div>
+				<div className="fotter-button_container">
+					<div>
+						<p>Do want to be part of us?</p>
+					</div>
+					<div>
+						<a
+							href="https://example.com/"
+							target="_blank"
+							rel="noreferrer"
+							className="footer-button"
+							onMouseEnter={() => setIsShown(true)}
+							onMouseLeave={() => setIsShown(false)}
+						>
+							Get started
+						</a>
+						{isShown && <div className="footer_hidden-show"> Get started</div>}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Footer1;
