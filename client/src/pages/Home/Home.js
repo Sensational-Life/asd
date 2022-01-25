ec7cd39b5ecbf00140f96d9ab5dd776b2351a72b
Megumi Yanaka <margaret_mori0102@yahoo.com>
2022-01-25 22:53:27 +0000

    implement suggestions for pr

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React from "react";
 import AboutUs from "./AboutUs/AboutUs";
 import FooterIcon from "./FooterIcon/FooterIcon";
 import Welcome from "./Welcome/Welcome";
@@ -6,31 +6,13 @@ import WhatWeDo from "./WhatWeDo/WhatWeDo";
 import WeSupportYou from "./WeSupportYou/WeSupportYou";
 
 export function Home() {
-	const [message, setMessage] = useState("Loading...");
-
-	useEffect(() => {
-		fetch("/api")
-			.then((res) => {
-				if (!res.ok) {
-					throw new Error(res.statusText);
-				}
-				return res.json();
-			})
-			.then((body) => {
-				setMessage(body.message);
-			})
-			.catch((err) => {
-				console.error(err);
-			});
-	}, []);
-
 	return (
 		<main role="main">
-				<Welcome />
-				<AboutUs />
-				<WhatWeDo />
-				<WeSupportYou />
-				<FooterIcon />
+			<Welcome />
+			<AboutUs />
+			<WhatWeDo />
+			<WeSupportYou />
+			<FooterIcon />
 		</main>
 	);
 }
