971fb3358bb4dbcd10c0ce85f15e8f7c6b772110
andrejskalnins <andrejs.kalnins@gmail.com>
2022-01-26 13:19:32 +0000

    deleted unecessary files

--- client/src/pages/Consulting.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Consulting() {
-  return (
-    <>
-      <h1 className="">timetables</h1>
-    </>
-  );
-}
--- client/src/pages/WhatIsAutism.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import '../../App.css';
-
-export default function WhatIsAutism() {
-  return <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>;
-}
