2a6de5ec80b2b7771f6eb16897e43c75b8e0a964
andrejskalnins <andrejs.kalnins@gmail.com>
2022-01-26 13:31:30 +0000

    cleaned up the code

--- client/src/components/Navbar/LoginIcon.js
+++ client/src/components/Navbar/LoginIcon.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./LoginIcon.css";
 
-import "./LoginIcon.css"
-
-function LoginIcon(){
-    return (
-			<Link className="login-icon" to="/login">
-				Login <i className="fas fa-user "></i>
-			</Link>
-		);
-    
+function LoginIcon() {
+	return (
+		<Link className="login-icon" to="/login">
+			Login <i className="fas fa-user "></i>
+		</Link>
+	);
 }
 export default LoginIcon;
