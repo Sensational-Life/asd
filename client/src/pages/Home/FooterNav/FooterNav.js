bdff2e17f88c69ccdeeac39d83248ba36bdcdc35
Megumi Yanaka <margaret_mori0102@yahoo.com>
2022-01-27 00:16:59 +0000

    style design

--- client/src/pages/Home/FooterNav/FooterNav.js
+++ client/src/pages/Home/FooterNav/FooterNav.js
@@ -1,31 +1,34 @@
 import React from "react";
+import { GrMail } from "react-icons/gr";
 import "./FooterNav.css";
 
 export function FooterNav() {
 	return (
 		<div id="footer">
-
 			<div className="home_section12_parent">
 				<div className="home_section12_child1">
-					<ul>
-						<li>List</li>
-						<li>List</li>
-						<li>List</li>
-					</ul>
-					<ul>
-						<li>List</li>
-						<li>List</li>
-						<li>List</li>
-					</ul>
-					<ul>
-						<li>List</li>
-						<li>List</li>
-						<li>List</li>
-					</ul>
+					<div className="nav-box">
+						<p className="title">About</p>
+						<a href="http://example.com/">Out Story</a>
+						<a href="http://example.com/">Team</a>
+					</div>
+					<div className="nav-box">
+						<p className="title">Resources</p>
+						<a href="http://example.com/">Timetables</a>
+						<a href="http://example.com/">Referrals</a>
+						<a href="http://example.com/">Useful Links</a>
+					</div>
+					<div className="nav-box">
+						<p className="title">Help</p>
+						<a href="http://example.com/">FAQs</a>
+					</div>
 				</div>
 				<div className="home_section12_child2">
 					<p>Our Email address</p>
-					<span>info@sensational-life.com</span>
+					<span>
+						<GrMail className="mail-icon" />
+						<a href="http://example.com/">info@sensational-life.com</a>
+					</span>
 				</div>
 			</div>
 		</div>
