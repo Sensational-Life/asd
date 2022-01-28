ac4263c5acf35eb1a30cc3300dc4a847e8fae876
Dilekcn <dcanturk1@gmail.com>
2022-01-28 23:13:17 +0000

    added link to buttom

--- client/src/pages/Home/WhatWeDo/WhatWeDo.js
+++ client/src/pages/Home/WhatWeDo/WhatWeDo.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import './WhatWeDo.css'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './WhatWeDo.css';
 
 function WhatWeDo() {
+  const [buttonShow, setButtonShow] =useState(false);
   return(
     <div className='whatwedo_main_container'>
-     
       <div className='whatwedo_container'>
           <div className='whatwedo_text_container'>
-           
             <div className='whatwedo_header'>
               <span >What We Do? </span>          
             </div>
@@ -16,9 +16,9 @@ function WhatWeDo() {
                products to help support parents of autistic <br />children on their autism journey
               within the <br />neurodiverse family.</span>                
             </div>
-            <button className='whatwedo_button'>Read More</button> 
+            <Link to="/whatwedo" onMouseEnter={()=>setButtonShow(true)} onMouseLeave={()=>setButtonShow(false)} ><button  className='whatwedo_button'>Read More</button> </Link><br/>
+             {buttonShow && <button  className='whatwedo_button_hover'>Read More</button>}
           </div>
-          
           <div className='whatwedo_img_container'>
               <div className='whatwedo_img_color'>
               <div className='whatwedo_img'><img src="https://i.postimg.cc/52Nj0XdD/cute-girl-with-down-syndrome-in-mothers-arms-2021-09-24-04-06-20-utc.jpg" alt='img'  /></div>
