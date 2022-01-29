df055fcf0e976c70a24a8affc01a0582bc6ed0b2
Tandy-Nicole <84181728+Tandy-Nicole@users.noreply.github.com>
2022-01-29 23:15:46 +0000

    Added class name for div, h4 p

--- client/src/pages/Home/MemberSays/Cards.js
+++ client/src/pages/Home/MemberSays/Cards.js
@@ -1,33 +1,14 @@
 import React from "react";
 import "./Member.css";
-
-const cardContent = [
-	{
-		Header: "Kate - UK",
-		Content:
-			"“Thank you so much this is great. I home school my daughter due to her high anxiety, so this will be used all time”",
-	},
-
-	{
-		Header: "Anonymous",
-		Content:
-			"“This group has given me the courage to go against the system and fight to get my daughter the help needs.”",
-	},
-
-	{
-		Header: "SENsational Life",
-		Content:
-			"“So happy that our closed parent support group has such an impact. Come on and over join us.”",
-	},
-];
+import cardContent from "./cardContent";
 
 export default function Cards() {
 	return (
 		<div className="grid-container">
-			{cardContent.map((x) => (
-				<div className="grid-item">
-					<h4>{x.Header}</h4>
-					<p>{x.Content}</p>
+			{cardContent.map((item, index) => (
+				<div className="grid-item" key={index}>
+					<h4 className="card-header">{item.Header}</h4>
+					<p className="card-text">{item.Content}</p>
 				</div>
 			))}
 		</div>
