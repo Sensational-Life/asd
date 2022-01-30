6f8bc25b6fe4756e3353177e255a10183a3a56ba
Dilekcn <dcanturk1@gmail.com>
2022-01-30 12:53:05 +0000

    font size changed from app.css

--- client/src/pages/Home/SubscribeNews/SubscribeNews.js
+++ client/src/pages/Home/SubscribeNews/SubscribeNews.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./SubscribeNews.css";
 import { FaChevronRight } from "react-icons/fa";
+import ImgWomen from "../../../image/woman-using-a-mobile-phone-in-the-kitchen-2021-08-30-13-43-41-utc-(1).jpg";
 
 function SubscribeNews() {
 
 
   const alertMess = ()=> {
-    return alert("Thank you very much for subscribing to our Newsletter.")
-  }
+    return alert("Thank you very much for subscribing to our Newsletter.");
+  };
 
 	return (
 		<div className="subscribeNews_container">
@@ -32,7 +33,7 @@ function SubscribeNews() {
 			<div className="subscribeNews_img_container">
 				<div className="subscribeNews_img">
 					<img
-						src="https://i.postimg.cc/65LfHnkV/woman-using-a-mobile-phone-in-the-kitchen-2021-08-30-13-43-41-utc-1.jpg"
+						src={ImgWomen}
 						alt="img"
 					/>
 				</div>
