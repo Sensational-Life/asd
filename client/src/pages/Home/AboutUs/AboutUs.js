9c2b3181f554d188cf0b20455287b04dbc506714
Dilekcn <dcanturk1@gmail.com>
2022-01-30 15:14:36 +0000

    classname changed

--- client/src/pages/Home/AboutUs/AboutUs.js
+++ client/src/pages/Home/AboutUs/AboutUs.js
@@ -1,30 +1,31 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './AboutUs.css'
+import AboutUsImg from '../../../assets/images/about-us-concept-2021-08-31-16-59-05-utc.jpg'
 
 function AboutUs() {
 
   const[buttonShow, setButtonShow] = useState(false)
 
   return (
-		<div className="Aboutus_main_container">
-			<div className="Aboutus_container">
-				<div className="Aboutus_img_container">
-					<div className="Aboutus_img_color">
-						<div className="Aboutus_img">
+		<div className="aboutus_main_container">
+			<div className="aboutus_container">
+				<div className="aboutus_img_container">
+					<div className="aboutus_img_color">
+						<div className="aboutus_img">
 							<img
-								src="https://i.postimg.cc/442yFsZD/about-us-concept-2021-08-31-16-59-05-utc.jpg"
+								src={AboutUsImg}
 								alt="img"
 							/>
 						</div>
 					</div>
 				</div>
 
-				<div className="Aboutus_text_container">
-					<div className="Aboutus_header">
+				<div className="aboutus_text_container">
+					<div className="aboutus_header">
 						<span>About Us </span>
 					</div>
-					<div className="Aboutus_text">
+					<div className="aboutus_text">
 						<span>
 							{" "}
 							SENsational Life: Autism and <br />
@@ -37,7 +38,7 @@ function AboutUs() {
 					</div>
 					<Link to="/about-us">
 						<button
-							className="Aboutus_button"
+							className="aboutus_button"
 							onMouseEnter={() => setButtonShow(true)}
 							onMouseLeave={() => setButtonShow(false)}
 						>
@@ -46,7 +47,7 @@ function AboutUs() {
 					</Link>{" "}
 					<br />
 					{buttonShow && (
-						<button className="Aboutus_button_hover">Read More</button>
+						<button className="aboutus_button_hover">Read More</button>
 					)}
 				</div>
 			</div>
