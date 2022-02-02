82a8b7b9aa387fde261414f249eb7a7bc4a5da61
Tandy-Nicole <84181728+Tandy-Nicole@users.noreply.github.com>
2022-02-02 20:16:22 +0000

    Added the hover button Added corresponding page link

--- client/src/pages/Home/MemberSays/MemberSays.js
+++ client/src/pages/Home/MemberSays/MemberSays.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Cards from "./Cards";
 import "./Member.css";
+import { Link } from "react-router-dom";
 
 function MemberSays() {
+	const [buttonShow, setButtonShow] = useState(false);
 	return (
 		<div className="memberSays-container ">
 			<h1 className="member-header">What Our Beloved Members Say</h1>
 			<Cards />
-			<button className="register-btn">Click here to Register</button>
+			<div className="register-btn-container">
+				<Link to="/register">
+					<button
+						className="register-btn"
+						onMouseEnter={() => setButtonShow(true)}
+						onMouseLeave={() => setButtonShow(false)}
+					>
+						Click here to Register
+					</button>
+				</Link>
+				{buttonShow && (
+					<button className="register-btn-hover ">
+						Click here to Register
+					</button>
+				)}
+			</div>
 		</div>
 	);
 }
