84cbd4b988a9796a2d35eff058cdae4b9731edba
Abd@94 <45979612+Abdalrazaq94@users.noreply.github.com>
2022-02-02 18:37:14 +0000

    Regiser-form

--- client/src/pages/RegisterPage/Register.js
+++ client/src/pages/RegisterPage/Register.js
@@ -1,8 +1,83 @@
-import React from 'react';
+import React,{ useState }  from "react";
+import "./Register.css";
+import { Link } from 'react-router-dom';
+import rigsterimg from '../RegisterPage/Regester image.png' 
+
 
 function Register() {
-  return <div>
-      Hello from register page
+  const [buttonShow, setButtonShow ] =useState(false);
+
+  return <div className='register-container'>
+
+             <div className='register-form-div'>
+                  <h6
+                        className='p-register'>
+                            Register with us
+                  </h6>
+                   {/*  Rigster form  */}
+
+                  <form className='form'>
+
+                      {/*Name  */}
+                     <input
+                         className='input-form'
+                          required
+                          id="name"
+                          type="text"
+                          placeholder="Nmae"
+                         name="Name"
+                            />
+
+                      {/*Email  */}
+                      <input
+                          className='input-form'
+                          required
+                           id="email"
+                          type="text"
+                          placeholder="Email"
+                          name="email"
+                            />
+                      {/*Password  */}
+                      <input
+                        className='input-form'
+                        required
+                       id="password"
+                       type="password"
+                       placeholder="Password"
+                       name="Password"
+                       />
+                      {/*Confirm-password */}
+                     <input
+                        className='input-form'
+                        required
+                        id="confirm-password"
+                        type="password"
+                        placeholder="Confirm-Password"
+                        name="confirm-Password"
+                        />
+                       {/* Register-Button */}
+						
+					          	<button className="Register_button"
+				              		onMouseEnter={() => setButtonShow(true)}
+				              		onMouseLeave={() => setButtonShow(false)}
+					          	> REGISTER</button>
+		
+				           	{buttonShow && <button className="Register_button_hover">REGISTER</button>}
+
+                  </form>
+              <div className='already-have-account-div'>
+
+                  <p>
+                     Already have an account? <span><Link  className='link-sgining' to={"/Login"}>Sign in</Link></span>
+                  </p>
+            </div>
+        </div>
+
+         {/* img/side/sectio */}
+       <div className='side-img'>
+         <img className="register_img" src={rigsterimg} alt="register-form" />
+      </div>
+
   </div>;
 }
 
