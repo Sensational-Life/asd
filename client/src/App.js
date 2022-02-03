1f6ff31f4cbcad8ad408dac919bcd7f8c066634b
Pavel <pavel.filkovskiy@gmail.com>
2022-02-03 23:07:05 +0000

    add route for OurEvents

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import "./App.css";
 import Resources from "./pages/Resources/Resources";
 import Contact from "./pages/Contact/Contact";
 import Login from "./pages/Login/Login";
+import OurEvents from "./pages/OurEvents/OurEvents";
 
 function App() {
 	return (
@@ -30,6 +31,7 @@ function App() {
 					<Route path="/resources" component={Resources} />
 					<Route path="/contact" component={Contact} />
 					<Route path="/login" component={Login} />
+					<Route path="/events" component={OurEvents} />
 					{/* Footer links: */}
 					<Route path="/about" component={AboutUs} />
 					<Route path="/termsConditions" component={TermsConditions} />
