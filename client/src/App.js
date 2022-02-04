ef5e835a09fe8fc5201b74fbbc383c0af0233f2e
Pavel <pavel.filkovskiy@gmail.com>
2022-02-04 12:33:40 +0000

    refactor code to make it more readable

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,28 @@
 import React from "react";
-import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// Navigation:
+import Navbar from "./components/Navbar/Navbar";
+// Home:
 import Home from "./pages/Home/Home";
-import AboutUs from "./pages/Home/AboutUs/AboutUs";
-import TermsConditions from "./pages/Terms&Condition/TermsConditions";
-import PrivacyPolicy from "./pages/PrivacyPolicy/PrivacyPolicy";
 import WhatWeDoMore from "./pages/WhatWeDoMore/WhatWeDoMore";
+// About:
 import About from "./pages/About/About";
-import Register from "./pages/RegisterPage/Register";
 import AboutUsMore from "./pages/AboutUSMore/AboutUsMore";
-import "./App.css";
-import Resources from "./pages/Resources/Resources";
-import Contact from "./pages/Contact/Contact";
-import Login from "./pages/Login/Login";
 import WhatIsAutism from "./pages/WhatIsAutism/WhatIsAutism";
 import AdditionalSupport from "./pages/AdditionalSupport/AdditionalSupport";
+// Resources:
+import Resources from "./pages/Resources/Resources";
+// Contacts:
+import Contact from "./pages/Contact/Contact";
+// Registration:
+import Register from "./pages/RegisterPage/Register";
+import Login from "./pages/Login/Login";
+// Footer links:
+import TermsConditions from "./pages/Terms&Condition/TermsConditions";
+import PrivacyPolicy from "./pages/PrivacyPolicy/PrivacyPolicy";
+// Styling:
+import "./App.css";
+
 
 function App() {
 	return (
@@ -22,28 +30,32 @@ function App() {
 			<Router>
 				<Navbar />
 				<Switch>
+
 					<Route path="/" exact component={Home} />
-					{/* routes from home page: */}
+
+					{/**
+					 * routes from home page:
+					 */}
 					<Route path="/register" component={Register} />
 					<Route path="/about-us-more" component={AboutUsMore} />
 					<Route path="/whatwedo" component={WhatWeDoMore} />
-					{/* routes from nav bar: */}
+
+					{/**
+					 * routes from nav bar:
+					 */}
 					<Route exact path="/about" component={About} />
-					<Route
-						path="/about/additional-support"
-						component={AdditionalSupport}
-					/>
-					<Route
-						path="/about/what-is-autism"
-						component={WhatIsAutism}
-					/>
+					<Route path="/about/additional-support" component={AdditionalSupport} />
+					<Route path="/about/what-is-autism" component={WhatIsAutism} />
 					<Route path="/resources" component={Resources} />
 					<Route path="/contact" component={Contact} />
 					<Route path="/login" component={Login} />
-					{/* Footer links: */}
-					<Route path="/about" component={AboutUs} />
+
+					{/**
+					 * Footer links:
+					*/}
 					<Route path="/termsConditions" component={TermsConditions} />
 					<Route path="/privacyPolicy" component={PrivacyPolicy} />
+
 				</Switch>
 			</Router>
 		</>
