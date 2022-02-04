97bfc2ab1a7e9c9e4032c77ffed565a75f2740e7
Pavel <pavel.filkovskiy@gmail.com>
2022-02-04 00:22:08 +0000

    render innerHTML from json

--- client/src/pages/OurEvents/OurEvents.js
+++ client/src/pages/OurEvents/OurEvents.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useLayoutEffect } from "react";
 import imgEvent from "~/assets/images/events-hero.jpg";
 
 import "./OurEvents.css";
@@ -8,6 +9,15 @@ import { FaAngleRight } from "react-icons/fa";
 const images = require.context("../../assets/images", true);
 
 function OurEvents() {
+	useLayoutEffect(() => {
+		// a bit ugly, but works...
+		Data.forEach((element, idx) => {
+			document
+				.querySelector(`div.ourevent_long_text[id='${idx}']`)
+				.innerHTML = element.text;
+		});
+	},[]);
+
 	return (
 		<div className="ourevent_container_main">
 			<div className="ourevent_img">
@@ -15,6 +25,8 @@ function OurEvents() {
 			</div>
 			<div className="container_cart">
 				{Data.map((event, i) => {
+					const text = document.createElement("div");
+					text.innerHTML = event.text;
 					return (
 						<div key={i} className="ourevent_container">
 							<div className="ourevent_small_img">
@@ -24,13 +36,13 @@ function OurEvents() {
 								<h3 className="ourevent_title">{event.title}</h3>
 								<p className="ourevent_date">{event.date}</p>
 							</div>
-							<div className="ourevent_long_text">
-								<p className="ourevent_text">{event.text}</p>
-							</div>
+							<div className="ourevent_long_text" id={i}></div>
 						</div>);
 					}
 				)}
-			<div className="container_scroll"> <FaAngleRight className="container_scroll_icon" /> </div>
+				<div className="container_scroll">
+					<FaAngleRight className="container_scroll_icon" />
+				</div>
 			</div>
 		</div>
 	);
