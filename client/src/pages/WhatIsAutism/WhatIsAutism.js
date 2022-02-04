ece09167b795db04bb5cb111d9d43690c8a63d32
Megumi Yanaka <margaret_mori0102@yahoo.com>
2022-02-04 10:05:06 +0000

    improve layout responsive

--- client/src/pages/WhatIsAutism/WhatIsAutism.js
+++ client/src/pages/WhatIsAutism/WhatIsAutism.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactPlayer from "react-player/youtube";
+import ResponsivePlayer from "react-player";
 import { BsPlayCircleFill } from "react-icons/bs";
 import whatIsAutismImg from "../../assets/images/what-is-autism.jpeg";
 import whatIsAutismVideoImg from "../../assets/images/what-is-autism-video.jpg";
@@ -11,58 +11,59 @@ import "./WhatIsAutism.css";
 function WhatIsAutism() {
 	return (
 		<div className="what-is-autism-container">
-			<img
-				className="hero-image"
-				src={whatIsAutismImg}
-				alt="A woman and a girl are talking"
-			></img>
-			<div className="main-container">
-				<div className="discription-container">
-					<div className="discription">
-						Autism or Autism Spectrum Disorder is not an illness or a disease.
-						Autism is not a medical condition with treatments or a “cure”. It
-						means your brain works in a different way from other people. It’s
-						something you’re born with or first appears when you’re very young.
-						<br />
-						Everyone with autism is different, that’s what makes it a spectrum.
-						Some people may need little or no support. Other may need extra help
-						to manage certain things such as daily tasks, activities, jobs or
-						relationships.
-					</div>
-					<div className="more-info-autism">
-						For more info and a guide about Autism <br />
-						visit the NHS website:
-						<a
-							href="https://www.nhs.uk/conditions/autism/"
-							target="_blank"
-							rel="noreferrer"
-						>
-							https://www.nhs.uk/conditions/autism/
-						</a>
-					</div>
+			<div className="what-is-autism-content">
+				<div className="what-is-autism-header">
+					<img src={whatIsAutismImg} alt="A woman and a girl are talking"></img>
 				</div>
-				<div className="video-container">
-					<div>
+
+				<div className="what-is-autism-section">
+					<div className="description-container">
+						<p>
+							Autism or Autism Spectrum Disorder is not an illness or a disease.
+							Autism is not a medical condition with treatments or a “cure”. It
+							means your brain works in a different way from other people.
+							<br />
+							It&rsquo;s something you&rsquo;re born with or first appears when
+							you&rsquo;re very young.
+							<br />
+							Everyone with autism is different, that&rsquo;s what makes it a
+							spectrum. Some people may need little or no support. Other may
+							need extra help to manage certain things such as daily tasks,
+							activities, jobs or relationships.
+						</p>
+						<p>
+							For more info and a guide about Autism <br />
+							visit the NHS website:
+							<a
+								href="https://www.nhs.uk/conditions/autism/"
+								target="_blank"
+								rel="noreferrer"
+								className="what-is-autism-link"
+							>
+								https://www.nhs.uk/conditions/autism/
+							</a>
+						</p>
+					</div>
+
+					<div className="video-container">
 						<div className="video-wrapper">
-							<ReactPlayer
+							<ResponsivePlayer
 								className="player"
-								url={"https://www.youtube.com/watch?v=0Pp8jcQ97pY"}
-								playing
+								url="https://www.youtube.com/watch?v=0Pp8jcQ97pY"
+								playing={true}
 								playIcon={<BsPlayCircleFill className="player-button" />}
 								controls={true}
 								light={whatIsAutismVideoImg}
 								width="100%"
-								height="130%"
+								height="100%"
 							/>
 						</div>
-					</div>
-					<div className="vide-discription">
-						<p>
-							Here is an informative video about Autism
-							<br />
-							Spectrum Disorder by The National Center <br />
-							for Learning Disabilities.
-						</p>
+						<div className="video-description">
+							<p>
+								Here is an informative video about Autism Spectrum Disorder by
+								The National Center for Learning Disabilities.
+							</p>
+						</div>
 					</div>
 				</div>
 			</div>
