ca94a6d14262a103a73721fc97b4907415a3ab65
Dilekcn <dcanturk1@gmail.com>
2022-02-05 00:31:18 +0000

    CSS refurbishment

--- client/src/pages/OurEvents/OurEvents.js
+++ client/src/pages/OurEvents/OurEvents.js
@@ -1,70 +1,62 @@
 import React, { useEffect, useState } from "react";
 import imgEvent from "~/assets/images/events-hero.jpg";
-
 import "./OurEvents.css";
 import Events from "./OurEvents.json";
-import { FaAngleRight , FaAngleLeft } from "react-icons/fa";
+import { FaAngleRight, FaAngleLeft } from "react-icons/fa";
 
 const images = require.context("../../assets/images", true);
 
 function OurEvents() {
-	const [start, setStart] = useState(0);
-	const [end, setEnd] = useState(3);
+	const [startLength, setStartLength] = useState(0);
+	const [endLength, setEndLength] = useState(3);
 	const [visibilityRight, setVisibilityRight] = useState(true);
-	const [visibilityLeft, setVisibilityLeft ] = useState(false);
+	const [visibilityLeft, setVisibilityLeft] = useState(false);
 
-	const next = (e) =>{
-
-		if(Events.length -1 == end) {
+	const forwardArrow = () => {
+		if (Events.length - 1 == endLength) {
 			setVisibilityRight(false);
 			return;
 		}
-
-
-		setStart(start + 1);
-		setEnd(end + 1);
+		setStartLength(startLength + 1);
+		setEndLength(endLength + 1);
 		setVisibilityLeft(true);
-
 	};
-	const back = (e) =>{
-		if(start === 0){
+	const backforwardArrow = () => {
+		if (startLength === 0) {
 			setVisibilityLeft(false);
 			return;
 		}
-
-		setStart(start - 1);
-		setEnd(end - 1);
-		if(Events.length -1 == end) {
+		setStartLength(startLength - 1);
+		setEndLength(endLength - 1);
+		if (Events.length - 1 == endLength) {
 			setVisibilityRight(true);
 		}
-
 	};
-
 	useEffect(() => {
 		const rightArrow = document.getElementById("ourevent_right_arrow");
 		const leftArrow = document.getElementById("ourevent_left_arrow");
 
-		rightArrow.style.visibility = visibilityRight ? "visible" : "hidden" ;
-		leftArrow.style.visibility  = visibilityLeft ? "visible" : "hidden" ;
-		if(visibilityLeft) {
+		rightArrow.style.visibility = visibilityRight ? "visible" : "hidden";
+		leftArrow.style.visibility = visibilityLeft ? "visible" : "hidden";
+		if (visibilityLeft) {
 			setVisibilityLeft(true);
 		}
-
-
-	},[visibilityLeft, visibilityRight]);
-
+	}, [visibilityLeft, visibilityRight]);
 
 	return (
 		<div className="ourevent_container_main">
 			<div className="ourevent_img">
 				<img src={imgEvent} alt="Event" />
 			</div>
-			<div className="container_cart">
-
-					<FaAngleLeft id="ourevent_left_arrow" onClick={back } className="container_scroll_icon" />
-
-
-				{Events.slice(start,end).map((event, i) => {
+			<div className="ourevent_container_cart">
+				<div className="ourevent_arrow_cont">
+					<FaAngleLeft
+						id="ourevent_left_arrow"
+						onClick={backforwardArrow}
+						className="ourevent_arrow ourevent_left_arrow"
+					/>
+				</div>
+				{Events.slice(startLength, endLength).map((event, i) => {
 					const text = document.createElement("div");
 					text.innerHTML = event.text;
 					return (
@@ -76,14 +68,20 @@ function OurEvents() {
 								<h3 className="ourevent_title">{event.title}</h3>
 								<p className="ourevent_date">{event.date}</p>
 							</div>
-							<div className="ourevent_long_text" dangerouslySetInnerHTML={{ __html: event.text }} />
-						</div>);
-					}
-				)}
-
-					<FaAngleRight  id="ourevent_right_arrow"  onClick={next } className="container_scroll_icon" />
-
-
+							<div
+								className="ourevent_long_text"
+								dangerouslySetInnerHTML={{ __html: event.text }}
+							/>
+						</div>
+					);
+				})}
+				<div className="ourevent_arrow_cont">
+					<FaAngleRight
+						id="ourevent_right_arrow"
+						onClick={forwardArrow}
+						className="ourevent_arrow ourevent_right_arrow"
+					/>
+				</div>
 			</div>
 		</div>
 	);
