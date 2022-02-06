ad72cf10aa6e5543fb00d0ea46535a2a1d768e1d
andrejskalnins <andrejs.kalnins@gmail.com>
2022-02-06 01:08:48 +0000

    add media queries

--- client/src/pages/Timetables/TimetableNotRegistered.js
+++ client/src/pages/Timetables/TimetableNotRegistered.js
@@ -7,37 +7,46 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import { FaCheck } from "react-icons/fa";
 import GirlImg from "~/assets/images/autistic-little-girl-close-up.jpg";
-// import arrowImg from "~/assets/images/dummyArrow.jpg";
-import leftTopImg from "~/assets/images/dummyLeft1.jpg";
-// import rightImg from "~/assets/images/dummyRight.jpg";
-import leftBottomImg from "~/assets/images/dummyLeftbottom.jpg";
-function TimetableNotRegistered(){
+import arrowImg from "~/images/dummyArrow.png";
+import leftTopImg from "~/images/dummyLeft1.jpg";
+import rightImg from "~/images/dummyRight.jpg";
+import leftBottomImg from "~/images/dummyLeftbottom.jpg";
 
+import FooterNav from "~/pages/Home/FooterNav/FooterNav";
+import FooterLogo from "~/pages/Home/FooterLogo/FooterLogo";
+import FooterIcon from "~/pages/Home/FooterIcon/FooterIcon";
+function TimetableNotRegistered(){
+const [buttonShow, setButtonShow] = useState(false);
     return (
 			<>
 				<main className="timetables-main-container">
 					<section className="timetables-content-container">
-						<section className="timetables-left-container">
-							<section className=" timestables-text-container">
-								<p>
-									Our visual timetables will help your Child to settle into a
-									routine with clear a clear structure. We also offer a guide
-									and a video that will teach you how to use visual timetable.
-								</p>
-								<p>
-									To access online or download <br></br> FREE Timetable{" "}
-									<Link to="/login">Sign Up.</Link>
-								</p>
-							</section>
+						<section className=" timestables-text-container">
+							<p>
+								Our visual timetables will help your Child to settle into a
+								routine with clear a clear structure. We also offer a guide and
+								a video that will teach you how to use visual timetable.
+							</p>
+							<p>
+								To access online or download <br></br> FREE Timetable{" "}
+								<Link to="/login">Sign Up.</Link>
+							</p>
 							<Link to="/register">
-								{" "}
-								<button className="register-button">
+								<button
+									className="register-button"
+									onMouseEnter={() => setButtonShow(true)}
+									onMouseLeave={() => setButtonShow(false)}
+								>
 									Click here to Register
 								</button>
 							</Link>
-							<img src={leftTopImg} alt="img" />
-							<img src={leftBottomImg} alt="img" />
+							{buttonShow && (
+								<button className="welcome_button_hover">
+									Click here to Register
+								</button>
+							)}
 						</section>
+
 						<section className="timetables-img-container">
 							<section className="timetables-img-color-blob">
 								<section className="img">
@@ -46,7 +55,18 @@ function TimetableNotRegistered(){
 							</section>
 						</section>
 					</section>
+					<section className="section2">
+						<div>
+							<img src={leftTopImg} alt="img" />
+							<img src={leftBottomImg} alt="img" />
+						</div>
+						<img className="arrowImg" src={arrowImg} alt="img" />
+						<img className="rightImg" src={rightImg} alt="img" />
+					</section>
 				</main>
+				<FooterLogo />
+				<FooterNav />
+				<FooterIcon />
 			</>
 		);
 }
