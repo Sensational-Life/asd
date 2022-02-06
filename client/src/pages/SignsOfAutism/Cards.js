23368d65a1adc33e7f2a619008c06994799cd40a
Megumi Yanaka <margaret_mori0102@yahoo.com>
2022-02-06 10:56:25 +0000

    format document

--- client/src/pages/SignsOfAutism/Cards.js
+++ client/src/pages/SignsOfAutism/Cards.js
@@ -7,33 +7,26 @@ export default function Cards() {
 		<div className="signs-of-autism-wrapper">
 			<div className="signs-of-autism-grid">
 				<div className="signs-of-autism-container">
-
 					<div className="cards-bg">
 						<div>
 							<h1 className="header">Signs of Autism</h1>
 						</div>
 					</div>
 					<div className="card-container">
-						{
-							cardContent.map((card) => {
-								const cardKey = card.title.toLowerCase().replace(/\s/ig, "");
-								return (
-									<div className="card" key={cardKey}>
-										<h3 className="card-header">{card.title}</h3>
-										<div dangerouslySetInnerHTML={{ __html: card.text }} />
-										<ul className="options">
-											{
-												card.options.map((option, idx) => {
-													return (
-														<li key={`${idx}_${cardKey}`}>{option}</li>
-													);
-												})
-											}
-										</ul>
-									</div>
-								);
-							})
-						}
+						{cardContent.map((card) => {
+							const cardKey = card.title.toLowerCase().replace(/\s/gi, "");
+							return (
+								<div className="card" key={cardKey}>
+									<h3 className="card-header">{card.title}</h3>
+									<div dangerouslySetInnerHTML={{ __html: card.text }} />
+									<ul className="options">
+										{card.options.map((option, idx) => {
+											return <li key={`${idx}_${cardKey}`}>{option}</li>;
+										})}
+									</ul>
+								</div>
+							);
+						})}
 					</div>
 				</div>
 			</div>
