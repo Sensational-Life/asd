5fd794aa895ca8222ee2c8fe700f18bba95de3ae
Megumi Yanaka <margaret_mori0102@yahoo.com>
2022-02-06 21:54:54 +0000

    remove fotter

--- client/src/pages/SignsOfAutism/SignsOfAutism.js
+++ client/src/pages/SignsOfAutism/SignsOfAutism.js
@@ -1,17 +1,11 @@
 import React from "react";
 import "./SignsOfAutism.css";
 import Cards from "./Cards";
-import FooterLogo from "../Home/FooterLogo/FooterLogo";
-import FooterNav from "../Home/FooterNav/FooterNav";
-import FooterIcon from "../Home/FooterIcon/FooterIcon";
 
 function SignsOfAutism() {
 	return (
 		<>
 			<Cards />
-			<FooterLogo />
-			<FooterNav />
-			<FooterIcon />
 		</>
 	);
 }
