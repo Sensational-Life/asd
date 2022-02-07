20e2537db91b3abeae895c27594d21cfd00a751b
Dilekcn <dcanturk1@gmail.com>
2022-02-07 01:22:03 +0000

    a

---
+++ client/src/pages/FrequentlyAskedQuestions/FrequentlyAskedQuestions.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./FrequentlyAskedQuestions.css";
+
+function FrequentlyAskedQuestions() {
+  return <div className='faQuestions_container'>
+      <h1>Frequently Asked Questions</h1>
+
+  </div>;
+}
+
+export default FrequentlyAskedQuestions;
+
---
+++ client/src/pages/Home/Faq/Faq.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Faq.css";
+
+function Faq() {
+	const [isShown, setIsShown] = useState(false);
+
+	return (
+		<div className="faq_container">
+			<div className="faq_button">
+				<Link
+                to="/faq"
+					className="faq_button_link"
+					onMouseEnter={() => setIsShown(true)}
+					onMouseLeave={() => setIsShown(false)}
+				>
+					Frequently Asked Questions
+				</Link>
+			</div>
+			{isShown && (
+				<div className="faq_button_tooltip"> Frequently Asked Questions</div>
+			)}
+		</div>
+	);
+}
+
+export default Faq;
+
