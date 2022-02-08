b0b04a1f0f90bcb2c8993195f6ad8a9b50422791
youssef910 <youssef.sadek910@gmail.com>
2022-02-08 20:45:39 +0000

    remove empty lined

--- server/api/index.js
+++ server/api/index.js
@@ -1,14 +1,8 @@
 import { Router } from "express";
 const router = Router();
 
-
-
 import users from "./users";
 import general from "./general";
-
-
-
-
 
 router.use("/users", users);
 router.use("/", general);
