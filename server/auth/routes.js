7e6d7d8677396a1ed4f5f9f25604c745213d4921
youssef910 <youssef.sadek910@gmail.com>
2022-02-09 23:41:24 +0000

    add register and login endpoint at auth

--- server/auth/routes.js
+++ server/auth/routes.js
@@ -11,16 +11,17 @@ router.post("/login", async (req, res, next) => {
 	console.log(`Login attempt ${email}`);
 	try {
 		const user = await db.getUserByEmail(email);
-
 		if (!email || !pwd || !user || pwd !== user.pwd) {
-			return res.sendStatus(403);
+			return res.status(400).send({
+				success: false,
+				message: "Email or Password doesn't Exist check your inputs",
+			});
 		}
-
 		const secret = process.env.JWT_SECRET || "your_jwt_secret";
 		const token = jwt.sign({ userId: user.id }, secret);
 		delete user.pwd;
 		delete user.salt;
-		return res.send({ token, user });
+		return res.send({ success: true, token, user });
 	} catch (e) {
 		console.error(e);
 		next(e);
@@ -32,26 +33,28 @@ router.post("/login", async (req, res, next) => {
  */
 router.post("/register", async (req, res, next) => {
 	const { name, email, pwd } = req.body;
-	const user = { name,  email, pwd };
-	console.log(user);
-db.getUserByEmail(email).then((data)=>
-	data&&data.length? res.send({
-				success: false,
-				message: "Account Already Exist",
-				data,
-			}):
-	db.createUser(user)
-		.then((data) => {
-			res.send({
-				success: true,
-				message: "Account created",
-				data,
-			});
-		})
-		.catch((err) => {
-			console.log(err);
-			next(err);
-		}));
+	const user = { name, email, pwd };
+
+	db.getUserByEmail(email).then((data) => {
+		data? res.status(400).send({
+					success: false,
+					message: "Account Already Exist",
+					data,
+			  })
+			: db
+					.createUser(user)
+					.then((data) => {
+						res.status(200).send({
+							success: true,
+							message: "Account created",
+							data,
+						});
+					})
+					.catch((err) => {
+						console.log(err);
+						next(err);
+					});
+	});
 });
 
 module.exports = router;
