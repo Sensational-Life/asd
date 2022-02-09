72728459db5a079f50284e1401097e5b23a1ed97
youssef910 <youssef.sadek910@gmail.com>
2022-02-09 23:39:50 +0000

    remove un used end points

--- server/api/users.js
+++ server/api/users.js
@@ -1,27 +1,18 @@
 const express = require("express");
 const router = express.Router();
-import { editPassword, getUserById } from "../Controller/services/repository/users.js";
+import {
+	editPassword,
+} from "../Controller/services/repository/users.js";
 
-router.get("/:id", (request, response) => {
-	const id = request.params.id;
-	getUserById(id)
-		.then((result) => {
-			result.length > 0
-				? response.status(200).send(result)
-				: response.sendStatus(400);
-		})
-		.catch((err) => {
-			console.error(err.message);
-		});
-});
 
 router.put("/change-password", (req, res) => {
 	const { userId, newPassword } = req.body;
 	editPassword(userId, newPassword)
-		.then((data) => res.send(data))
-		.catch((err) => {
-			console.error(err.message);
-			res.status(500);
-		});
+	.then((data) => res.send(data))
+	.catch((err) => {
+		console.error(err.message);
+		res.status(500);
+	});
 });
+
 export default router;
