21a1427b8ed08b21ec7b5d51d16930256324380e
youssef910 <youssef.sadek910@gmail.com>
2022-02-09 23:39:31 +0000

    remove unused  routes

--- server/api/index.js
+++ server/api/index.js
@@ -2,9 +2,7 @@ import { Router } from "express";
 const router = Router();
 
 import users from "./users";
-import general from "./general";
 
 router.use("/users", users);
-router.use("/", general);
 
 export default router;
