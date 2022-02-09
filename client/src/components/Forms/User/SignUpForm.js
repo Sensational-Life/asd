7eb3eff564865e8572068947ec5da56e9c381d61
Abd@94 <45979612+Abdalrazaq94@users.noreply.github.com>
2022-02-09 01:19:31 +0000

    adding new input for ConfirmPassword

--- client/src/components/Forms/User/SignUpForm.js
+++ client/src/components/Forms/User/SignUpForm.js
@@ -5,68 +5,73 @@ import "./UserForms.css";
 function SignUpForm() {
 	const [showSubmitBtnToolTip, setShowSubmitBtnToolTip] = useState(false);
 	return (
-        <div className="user-form-container">
-				<div className="user-form-header">
-					<h1>Register with us</h1>
+		<div className="user-form-container">
+			<div className="user-form-header">
+				<h1>Register with us</h1>
+			</div>
+
+			<form className="user-form">
+				<input
+					className="text-field"
+					required
+					id="name"
+					type="text"
+					placeholder="Name"
+					name="name"
+				/>
+
+				<input
+					className="text-field"
+					required
+					id="email"
+					type="text"
+					placeholder="Email"
+					name="email"
+				/>
+
+				<input
+					className="text-field"
+					required
+					id="password"
+					type="password"
+					placeholder="Password"
+					name="Password"
+				/>
+
+				<input
+					className="text-field"
+					required
+					id="Confirm-password"
+					type="password"
+					placeholder="Confirm Password"
+					name="Confirm-Password"
+				/>
+
+				<button
+					className="submit-button"
+					onMouseEnter={() => setShowSubmitBtnToolTip(true)}
+					onMouseLeave={() => setShowSubmitBtnToolTip(false)}
+				>
+					Register
+				</button>
+
+				<div className="submit-button-tooltip-container">
+					{showSubmitBtnToolTip && (
+						<button className="submit-button-tooltip">Register</button>
+					)}
 				</div>
 
-				<form className="user-form">
-					<input
-						className="text-field"
-						required
-						id="name"
-						type="text"
-						placeholder="Name"
-						name="name"
-					/>
-
-					<input
-						className="text-field"
-						required
-						id="email"
-						type="text"
-						placeholder="Email"
-						name="email"
-					/>
-
-					<input
-						className="text-field"
-						required
-						id="password"
-						type="password"
-						placeholder="Password"
-						name="Password"
-					/>
-
-					<button
-						className="submit-button"
-						onMouseEnter={() => setShowSubmitBtnToolTip(true)}
-						onMouseLeave={() => setShowSubmitBtnToolTip(false)}
-					>Register</button>
-
-					<div className="submit-button-tooltip-container">
-						{
-							showSubmitBtnToolTip && (
-								<button className="submit-button-tooltip">Register</button>
-						)}
-					</div>
-
-					<div className="user-form-footer">
-						<p>
-							<span className="text">
-								Already have an account?
-							</span>
-							{" "}
-							<span className="link">
-								<Link to="/signin">
-									Sign In
-								</Link>
-							</span>
-						</p>
-					</div>
-				</form>
-			</div>
-    );
+				<div className="user-form-footer">
+					<p>
+						<span className="text">Already have an account?</span>{" "}
+						<span className="link">
+							<Link to="/signin">Sign In</Link>
+						</span>
+					</p>
+				</div>
+			</form>
+		</div>
+	);
 }
 
 export default SignUpForm;
