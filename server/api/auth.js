711e221450c2dcb9d4aa206ceb2787d9caf60bd1
youssef910 <youssef.sadek910@gmail.com>
2022-02-11 19:23:16 +0000

    move auth file in api folder  and add it to index

--- server/api/auth.js
+++ server/api/auth.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const db = require("../Controller/services/repository/users");
+const {
+	getUserByEmail,
+	createUser
+} = require("../Controller/services/repository/users");
 
 /**
  * Users Login
@@ -10,11 +13,11 @@ router.post("/login", async (req, res, next) => {
 	const { email, pwd } = req.body;
 	console.log(`Login attempt ${email}`);
 	try {
-		const user = await db.getUserByEmail(email);
+		const user = await getUserByEmail(email);
 		if (!email || !pwd || !user || pwd !== user.pwd) {
 			return res.status(400).send({
 				success: false,
-				message: "Email or Password doesn't Exist check your inputs",
+				message: "Email or Password doesn't Exist check your inputs"
 			});
 		}
 		const secret = process.env.JWT_SECRET || "your_jwt_secret";
@@ -35,19 +38,19 @@ router.post("/register", async (req, res, next) => {
 	const { name, email, pwd } = req.body;
 	const user = { name, email, pwd };
 
-	db.getUserByEmail(email).then((data) => {
-		data? res.status(400).send({
+	getUserByEmail(email).then((data) => {
+		data
+			? res.status(400).send({
 					success: false,
 					message: "Account Already Exist",
-					data,
+					data: data
 			  })
-			: db
-					.createUser(user)
+			: createUser(user)
 					.then((data) => {
 						res.status(200).send({
 							success: true,
 							message: "Account created",
-							data,
+							data
 						});
 					})
 					.catch((err) => {
--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,8 @@ import { Router } from "express";
 const router = Router();
 
 import users from "./users";
-
+import  auth  from "./auth";
 router.use("/users", users);
+router.use("/auth",auth);
 
 export default router;
