185dcccf83e3dead6803bbc3d4c66e9348231807
Megumi Yanaka <margaret_mori0102@yahoo.com>
2022-02-11 11:21:55 +0000

    improve layout in each card

--- client/src/pages/Resources/Timetable/Registered/TimetableRegistered.js
+++ client/src/pages/Resources/Timetable/Registered/TimetableRegistered.js
@@ -19,13 +19,14 @@ function TimetableRegistered() {
 								<h1 className="header">Resources</h1>
 							</div>
 						</div>
-						<div className="card-container">
+						<div className="cards-container">
 							{CardContent.map((card) => {
 								return (
-									<div className="card" key={card.id}>
-										<h3 className="card-header">{card.title}</h3>
+									<div className="card-container" key={card.id}>
+										<div className="card-header">{card.title}</div>
+										<div className="card-items">
 										<div dangerouslySetInnerHTML={{ __html: card.text }} />
-										<div>
+										<div className="card-image-container">
 											<img
 												className="card-image"
 												src={card.src}
@@ -37,6 +38,7 @@ function TimetableRegistered() {
 											<a href={card.pdflink}>
 												<BsDownload className="timetable-icon-download" />
 											</a>
+										</div>
 										</div>
 									</div>
 								);
