902f43f903f4381db83e5c535377b7f21c9a15e7
Pavel <pavel.filkovskiy@gmail.com>
2022-02-12 19:17:54 +0000

    refactor styling

--- client/src/pages/JoinUs/JoinUs.js
+++ client/src/pages/JoinUs/JoinUs.js
@@ -1,33 +1,31 @@
 import React from "react";
-import "./JoinUs.css";
+import { Link } from "react-router-dom";
 import JoinUsimg from "~/assets/images/Join-us.jpg";
+import "./JoinUs.css";
 
 function JoinUs() {
 	return (
 		<div className="join-us-container">
-			<div className="join-us-container-img">
-				<div className="join-us-img-color">
-					<div className="join-us-img">
-						<img className="join-us-main-img" src={JoinUsimg} alt="JoinUs" />
+			<div className="content">
+				<div className="image-container">
+					<div className="image-shadow">
+						<img src={JoinUsimg} alt="JoinUs" />
 					</div>
 				</div>
-			</div>
-			<div className="join-us-container-text-header">
-				<div className="join-us-text-header">
-					<h5 className="join-us-tilte-header">Come along!</h5>
-					<p className="join-us-p">
-						Come and join our FREE parent support group - a great online support
-						community, with regular guest speakers, advice from autistic members
-						along with non - judgemental fellow parents who ‘get it’!
-					</p>
-					<h6 className="join-us-checkout-text">
-						<a
-							className="join-us-link-faceBook"
-							href="https://www.facebook.com/groups/autismandparentsupport/"
-						>
-							Check Out Our FREE Facebook Support Group.
-						</a>
-					</h6>
+				<div className="text-container">
+					<div className="wrapper">
+						<h1 >Come along!</h1>
+						<p>
+							Come and join our FREE parent support group - a great online support
+							community, with regular guest speakers, advice from autistic members
+							along with non - judgemental fellow parents who &apos;get it&apos;!
+						</p>
+						<p>
+							<Link to="https://www.facebook.com/groups/autismandparentsupport">
+								Check Out Our FREE Facebook Support Group.
+							</Link>
+						</p>
+					</div>
 				</div>
 			</div>
 		</div>
