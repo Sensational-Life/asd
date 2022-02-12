9c8c9bf22bfb2ddc8e99b2e07a758b4b4763c962
Abd@94 <45979612+Abdalrazaq94@users.noreply.github.com>
2022-02-12 15:32:42 +0000

    feature/uesful-links-hotfix-Css-style

--- client/src/pages/Resources/UesfulLinks/UesfulLinks.js
+++ client/src/pages/Resources/UesfulLinks/UesfulLinks.js
@@ -4,47 +4,47 @@ import UesfulLinkImg from "~/assets/images/sea.jpg";
 
 function UesfulLinks() {
 	return (
-		<div className="UesfulLink-container">
-			<div className="UesfulLinks-img-conatiner">
-				<div className="UesfulLinks-img-color">
+		<div className="uesful-links-container">
+			<div className="uesful-links-img-conatiner">
+				<div className="uesflu-links-img-color">
 					<img
-						className="UesfulLink-img"
+						className="uesful-links-img"
 						src={UesfulLinkImg}
 						alt="Uesful-link"
 					/>
 				</div>
 			</div>
-			<div className="UesfulLinks-text-links-conatiner">
-				<div className="UesfulLink-hesdr-text">
-					<h1 className="UesfulLink-text"> Useful Links</h1>
-					<div className="UesfulLinks-links">
-						<div className="U-links">
-							<p className="UesfulLink-p">Autism Network Scotland </p>
+			<div className="uesful-link-text-conatiner">
+				<div className="uesful-links-hesdr-text">
+					<h1 className="uesful-links-text"> Useful Links</h1>
+					<div className="uesful-links-links">
+						<div className="uesful-main-links">
+							<p className="uesful-links-p">Autism Network Scotland </p>
 							<a
-								className="UesfulLinks-a"
+								className="uesful-links-a"
 								href="https://www.autismnetworkscotland.org.uk/pages/home"
 							>
 								https://www.autismnetworkscotland.org.uk/pages/home
 							</a>
 						</div>
-						<div className="U-links">
-							<p className="UesfulLink-p">Scottish Autism </p>
+						<div className="uesful-main-links">
+							<p className="uesful-links-p">Scottish Autism </p>
 							<a
-								className="UesfulLinks-a"
+								className="uesful-links-a"
 								href="https://www.scottishautism.org/"
 							>
 								https://www.scottishautism.org/
 							</a>
 						</div>
-						<div className="U-links">
-							<p className="UesfulLink-p">National Autistic Society </p>
-							<a className="UesfulLinks-a" href="https://www.autism.org.uk/ ">
+						<div className="uesful-main-links">
+							<p className="uesful-links-p">National Autistic Society </p>
+							<a className="uesful-links-a" href="https://www.autism.org.uk/ ">
 								https://www.autism.org.uk/
 							</a>
 						</div>
-						<div className="U-links">
-							<p className="UesfulLink-p">Different minds One Scotland </p>
-							<a className="UesfulLinks-a" href="https://differentminds.scot/">
+						<div className="uesful-main-links">
+							<p className="uesful-links-p">Different minds One Scotland </p>
+							<a className="uesful-links-a" href="https://differentminds.scot/">
 								https://differentminds.scot/
 							</a>
 						</div>
