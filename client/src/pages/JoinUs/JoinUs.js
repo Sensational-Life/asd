372070dd97b8375f7c18fe2002f8c8591c63dc09
Abd@94 <45979612+Abdalrazaq94@users.noreply.github.com>
2022-02-12 16:33:26 +0000

    feature/join-us-fotfix-Css-style

--- client/src/pages/JoinUs/JoinUs.js
+++ client/src/pages/JoinUs/JoinUs.js
@@ -4,25 +4,25 @@ import JoinUsimg from "~/assets/images/Join-us.jpg";
 
 function JoinUs() {
 	return (
-		<div className="JoinUs-container">
-			<div className="JoinUs-container-img">
-				<div className="JoinUs-img-color">
-					<div className="JoinUs-img">
-						<img className="JoinUs-main-img" src={JoinUsimg} alt="JoinUs" />
+		<div className="join-us-container">
+			<div className="join-us-container-img">
+				<div className="join-us-img-color">
+					<div className="join-us-img">
+						<img className="join-us-main-img" src={JoinUsimg} alt="JoinUs" />
 					</div>
 				</div>
 			</div>
-			<div className="JoinUs-container-text-header">
-				<div className="JoinUs-text-header">
-					<h5 className="JoinUs-tilte-header">Come along!</h5>
-					<p className="JoiunUs-p">
+			<div className="join-us-container-text-header">
+				<div className="join-us-text-header">
+					<h5 className="join-us-tilte-header">Come along!</h5>
+					<p className="join-us-p">
 						Come and join our FREE parent support group - a great online support
 						community, with regular guest speakers, advice from autistic members
 						along with non - judgemental fellow parents who ‘get it’!
 					</p>
-					<h6 className="Joinus-checkout-text">
+					<h6 className="join-us-checkout-text">
 						<a
-							className="Join-link-faceBook"
+							className="join-us-link-faceBook"
 							href="https://www.facebook.com/groups/autismandparentsupport/"
 						>
 							Check Out Our FREE Facebook Support Group.
