6b082af8092bd8e2a8246dca3e8e05417b8b86dc
Pavel <pavel.filkovskiy@gmail.com>
2022-02-18 10:42:19 +0000

    fix styling

--- client/src/pages/Resources/Edutainment/Edutainment.js
+++ client/src/pages/Resources/Edutainment/Edutainment.js
@@ -6,23 +6,28 @@ const Edutainment = () => {
 	return (
 		<div>
 			<div className="edutainment-text-container">
-				<div className="header-column">
-					Edutainment
-					<br /> services
-				</div>
-				<div className="description-column">
-					Our unique Edutainment services also offers, a combination of
-					entertainment with education through listed videos for toddlers and
-					young children. These videos can be used in a number of ways such a as
-					helping a child to go to bed or perform a certain task.
-				</div>
-				<div className="feel-free-column">
-					<h3>
-						Feel free to browse
-						<br />
-						through our videos.
-					</h3>
-					<h3>Enjoy!</h3>
+				<div className="edutainment-text-grid">
+					<div className="text-column header-column">
+						Edutainment
+						<br /> services
+					</div>
+					<div className="text-column description-column">
+						Our unique Edutainment services also offers, a combination of
+						entertainment with education through listed videos for toddlers and
+						young children. These videos can be used in a number of ways such a as
+						helping a child to go to bed or perform a certain task.
+					</div>
+					<div className="text-column feel-free-column">
+						<p>
+							Feel free to browse
+							<br />
+							through our videos.
+						<p>
+
+						</p>
+							Enjoy!
+						</p>
+					</div>
 				</div>
 			</div>
 			<div>
